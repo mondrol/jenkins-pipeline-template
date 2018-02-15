52f5476d283926053f548d6fd0de7fc1d2969dcf
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-02-15 16:34:05 -0200

    Add contextDir to template

--- create-job.sh
+++ create-job.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+case "$1" in
+  -h|--help) echo Usage: $0 job-name jenkins-hostname git-uri git-ref output-image; exit 0
+esac
+
 set -eu
 
 export JOB_NAME=$1
 export JENKINS_HOSTNAME=$2
-export SOURCE_GIT_URI=$3
+export SOURCE_GIT_URI=${3%%#*}
+export SOURCE_GIT_DIR=${3##*#}
+if [ -z "$SOURCE_GIT_DIR" -o "$SOURCE_GIT_URI" == "$SOURCE_GIT_DIR" ]; then
+    export SOURCE_GIT_DIR=/
+fi
 export SOURCE_GIT_REF=$4
 export OUTPUT_IMAGE=$5
 
