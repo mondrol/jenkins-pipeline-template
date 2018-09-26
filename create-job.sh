4048505071fb0be0f3b96e656335afdc54f53689
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-09-26 11:44:24 -0300

    Update create-job.sh

--- create-job.sh
+++ create-job.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 case "$1" in
-  -h|--help) echo Usage: $0 job-name jenkins-hostname git-uri git-ref output-image; exit 0
+  -h|--help) echo Usage: $0 job-name-prefix jenkins-hostname git-uri git-ref output-image; exit 0
 esac
 
 set -eu
 
-export JOB_NAME=$1
 export JENKINS_HOSTNAME=$2
 export SOURCE_GIT_URI=${3%%#*}
 export SOURCE_GIT_DIR=${3##*#}
@@ -16,6 +15,9 @@ fi
 export SOURCE_GIT_REF=$4
 export OUTPUT_IMAGE=$5
 export IMAGE_TAG=${OUTPUT_IMAGE##*:}
+[ -n "$IMAGE_TAG" ] || export IMAGE_TAG=latest
+
+export JOB_NAME="$1-$IMAGE_TAG"
 
 envsubst < job.xml.template > job.xml
 
